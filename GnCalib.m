91bd20b32ea8114d2991343a186262be05841236
Sepehr Ramezani <ramezani.dan@gmail.com>
2021-07-13 15:26:13 -0400

    Made the code more clear to read

--- GnCalib.m
+++ GnCalib.m
@@ -1,6 +1,6 @@
-function [Ph,Pk,Pa]= GnCalib (dfolder,subjectname,plotflage)
+function [P_Gonio_H,P_Gonio_K,P_Gonio_A]= GnCalib (Datafolder,psname,plotflage)
 
-Fname =append(subjectname,"_GnCalib_");
+Fname =append(psname,"GnCalib_");
 Names=["AnkleDorsi15","AnkleDorsi10","Ankle0","AnklePlant10","AnklePlant30","AnklePlant50"...
     ,"Hip0","Hip30","Hip60","Hip90"...
     ,"Knee0","Knee30","Knee45","Knee60","Knee90"];
@@ -9,7 +9,7 @@ HipGnCalibdata=[];
 KneeGnCalibdata=[];
 
 for i=1:length(Names)
-    GnCaldatadir=importdata(append(dfolder,"\",Fname,Names(i),".csv"));
+    GnCaldatadir=importdata(append(Datafolder,"\",Fname,Names(i),".csv"));
     [Gr,Gc]=find(strncmp(GnCaldatadir.textdata,'Gn',2));
     %     [Tr,Tc]=find(strncmp(GnCaldatadir.textdata,'X',1));
     Tc=[1,3];
@@ -33,38 +33,44 @@ end
 y=[15,10,0,-10,-30,-50];
 y=y./180*pi();
 x=AnkelGnCalibdata;
-Pa = polyfit(x(:,2)',y,1);
-y1 = polyval(Pa,x(:,2));
+P_Gonio_A = polyfit(x(:,2)',y,1);
+y1 = polyval(P_Gonio_A,x(:,2));
 if plotflage
     plot(x(:,1),y,'*',x(:,2),y,'*',x(:,2),y1);
-    legend('A','B')
-    title('Ankle')
+    xlabel('Biodex Output (Voltage)');
+    ylabel('Angle (Degree)');
+    legend('Ch A','Ch B');
+    title('Goinometer Calibration Data for Ankle');
 end
 
 
-y=[0,30,60,90];
-y=y./180*pi();
+ydeg=[0,30,60,90];
+y=ydeg./180*pi();
 x=HipGnCalibdata;
-Ph = polyfit(x(:,2)',y,2);
-y1 = polyval(Ph,x(:,2));
+P_Gonio_H = polyfit(x(:,2)',y,2);
+y1 = polyval(P_Gonio_H,x(:,2));
 if plotflage
     figure
-    plot(x(:,1),y,'*',x(:,2),y,'*',x(:,2),y1);
-    legend('A','B')
-    title('Hip')
+    plot(x(:,1),ydeg,'*',x(:,2),ydeg,'*',x(:,2),y1*180/3.14);
+    xlabel('Biodex Output (Voltage)');
+    ylabel('Angle (Degree)');
+    legend('Ch A','Ch B');
+    title('Goinometer Calibration Data for Hip');
 end
 
 
-y=[0,30,45,60,90];
+y=[0,0,45,60,90];
 y=y./180*pi();
-x=KneeGnCalibdata;
-Pk = polyfit(x(:,2)',y,1);
-y1 = polyval(Pk,x(:,2));
+x=[KneeGnCalibdata(1,:);KneeGnCalibdata([1,3,4,5],:)];
+P_Gonio_K = polyfit(x(:,2)',y,2);
+y1 = polyval(P_Gonio_K,x(:,2));
 if plotflage
     figure
     plot(x(:,1),y,'*',x(:,2),y,'*',x(:,2),y1);
-    legend('A','B')
-    title('Knee')
+    xlabel('Biodex Output (Voltage)');
+    ylabel('Angle (Degree)');
+    legend('Ch A','Ch B');
+    title('Goinometer Calibration Data for Knee');
 end
 
 end
