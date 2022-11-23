c8b1addc12257b2dd83db2d3dcf40ad451288ce9
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2022-11-23 16:51:36 -0500

    making new model of subject base on optimized parameter

--- changemodelproperty.m
+++ changemodelproperty.m
@@ -1,12 +1,36 @@
-function [osimmodel]=changemodelproperty(osimmodel,Data)
+function [osimmodel]=changemodelproperty(osimmodel,Coordlable,Data,Optimizedflag)
 import org.opensim.modeling.*;
-for i=0:1:osimmodel.getMuscles().getSize()-1
+OptData=importdata(Data.(Coordlable).ParamSimulPath);
+if Optimizedflag
+    Muscname=Data.SimMusclename;
+    for m=1:1:osimmodel.getMuscles().getSize()-1
+        M_indx=find(contains(OptData.colheaders,Muscname(m))&~contains(OptData.colheaders,'force'));
+        CurrentMuscle=osimmodel.getMuscles().get(Muscname(m));
+        for y=1:length(M_indx)          
+            if contains(OptData.colheaders(M_indx(y)),'tendon_slack')
+                OptTSL=OptData.data(1,M_indx(y));
+                DeGrooteFregly2016Muscle.safeDownCast(CurrentMuscle).set_tendon_slack_length(OptTSL);
+            elseif contains(OptData.colheaders(M_indx(y)),'passive_fiber_')
+                OptPassiveFiber=OptData.data(1,M_indx(y));
+                DeGrooteFregly2016Muscle.safeDownCast(CurrentMuscle).set_passive_fiber_strain_at_one_norm_force(OptPassiveFiber);
+            elseif contains(OptData.colheaders(M_indx(y)),'tendon_strain')
+                OptTendonStrain=OptData.data(1,M_indx(y));
+                DeGrooteFregly2016Muscle.safeDownCast(CurrentMuscle).set_tendon_strain_at_one_norm_force(OptTendonStrain);
+            end
+        end
+        
+    end
+    osimmodel.initSystem();
+    osimmodel.print(insertBefore(char(Data.(Coordlable).ModelPath),".osim","_optimized"));
+else
+    for i=0:1:osimmodel.getMuscles().getSize()-1
         CurrentMuscle=osimmodel.getMuscles().get(i);
-%         CurrentMuscle.set_tendon_slack_length();
+        %         CurrentMuscle.set_tendon_slack_length();
         RandomTendonStrain=Data.TendonStrainBound(1) + (diff(Data.TendonStrainBound)).*rand();
         DeGrooteFregly2016Muscle.safeDownCast(CurrentMuscle).set_tendon_strain_at_one_norm_force(RandomTendonStrain);
         RandomPassiveFiber=Data.PassiveFiberBound(1) + (diff(Data.PassiveFiberBound)).*rand();
         DeGrooteFregly2016Muscle.safeDownCast(CurrentMuscle).set_passive_fiber_strain_at_one_norm_force(RandomPassiveFiber);
         osimmodel.initSystem();
+    end
 end
 end
