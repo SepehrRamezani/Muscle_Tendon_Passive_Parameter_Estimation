1009034ec02c0aa87b5c6d1ea2a7f9f915aa4250
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2022-12-18 22:41:38 -0500

    make reshaping function the more compatible
    If all delsys sensor was Avanti there was a one time series for whole data but since we use different type of sensors (avanti and trigno) we have different sample time. new code can handle all scenarios including different type of sensors or only one type.

--- ReshapingData.m
+++ ReshapingData.m
@@ -1,39 +1,60 @@
 function [Gdata,Gheader]= ReshapingData(data,DStime)
 Gdata=[0];
-for ii=2:2:length(data.textdata)
+Gheader=[];
+timeinx=find(contains(data.textdata,"X [s]"));
+
+for ii=1:length(timeinx)
     kk=1;
     tflage=0;
     ww=0;
     for jj=1:length(data.data)%# of data points in a given trial
-        if ~isnan(data.data(jj,ii))&&data.data(jj,ii)~=0
+        if ~isnan(data.data(jj,timeinx(ii)+1))&&data.data(jj,timeinx(ii)+1)~=0
             kk=jj;  %finding zero data at the end of each chanel
             tflage=1;
         elseif tflage==0
             ww=jj;  %finding zero data at the begining
         end
     end
-    ts(ii/2)=data.data(ww+1,ii-1); % first time of first chanel to set as final time for every other channel.
-    te(ii/2)=data.data(kk,ii-1); % final time of first chanel to set as final time for every other channel.
+    ts(ii)=data.data(ww+1,timeinx(ii)); % first time of first chanel to set as final time for every other channel.
+    te(ii)=data.data(kk,timeinx(ii)); % final time of first chanel to set as final time for every other channel.
 end
 %%             finding the highest starting
 tsfinal=max(ts);
 tefinal=min(te);
 interpolatetime=tsfinal:DStime:tefinal;
-for ii=2:2:length(data.textdata)
+for ii=1:length(timeinx)
     
-    starttimeinx=find(data.data(:,ii-1)>= tsfinal & data.data(:,ii-1)<=tefinal);
-    y=interp1(data.data(starttimeinx,ii-1),data.data(starttimeinx,ii),interpolatetime,'linear','extrap'); %Interpolates data to match sampling time to desierd sampling time
-    
-    b=y';
+    starttimeinx=find(data.data(:,timeinx(ii))>= tsfinal & data.data(:,timeinx(ii))<=tefinal);
+    if ii==length(timeinx)
+        y=interp1(data.data(starttimeinx,timeinx(ii)),data.data(starttimeinx,[timeinx(ii)+1:end]),interpolatetime,'linear','extrap'); %Interpolates data to match sampling time to desierd sampling time
+    else
+        y=interp1(data.data(starttimeinx,timeinx(ii)),data.data(starttimeinx,[timeinx(ii)+1:timeinx(ii+1)-1]),interpolatetime,'linear','extrap'); %Interpolates data to match sampling time to desierd sampling time
+    end
+    [ry,cy]=size(y);
+    if ry==1
+        b=y';
+    else
+        b=y;
+    end
     %                 ends(ii)=length(b);
     if (size(Gdata(:,1)) == 1) %recombines data into a matrix padded with NaN
         Gdata = [interpolatetime' b];
+        if ii==length(timeinx)
+            Gheader=["time" data.textdata([timeinx(ii)+1:end])];
+        else
+            Gheader=["time" data.textdata([timeinx(ii)+1:timeinx(ii+1)-1])];
+        end
     else
         Gdata = [Gdata b];
+        if ii==length(timeinx)
+            Gheader=[Gheader data.textdata(timeinx(ii)+1:end)];
+        else
+            Gheader=[Gheader data.textdata(timeinx(ii)+1:timeinx(ii+1)-1)];
+        end
     end
-    
+  
 end
 
-Gheader=["time" data.textdata(2:2:end)];
+
 
 end
