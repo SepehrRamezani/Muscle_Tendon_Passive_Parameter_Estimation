50b08670658b0cc96da0908760b32ffce3212fee
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2022-12-28 20:54:03 -0500

    Torque signal is trimmed by max torque
    Event detection is edited to cut the signal base of the maximum torque

--- EventDetection.m
+++ EventDetection.m
@@ -1,7 +1,8 @@
-function Events = EventDetection(DStime,Data,Threshold)
+function Events = EventDetection(DStime,Data,Threshold,TorqueFlage)
 
-    [bb,aa] = butter(4, 0.2,'low');
-    DataFiltered=filtfilt(bb,aa,Data);
+
+    [bb,aa] = butter(1, 0.4,'low');
+    DataFiltered=filtfilt(bb,aa,Data(:,1));
     normDataFiltered=DataFiltered/max(DataFiltered);
     Velocity=abs(diff(normDataFiltered)/DStime);
     Stimeindx=find(Velocity<=Threshold);
@@ -9,13 +10,18 @@ function Events = EventDetection(DStime,Data,Threshold)
     ConstantTime=Stimeindx(1:selecttime(1)-1);
     Etimeindx=find(Velocity > Threshold);
     selecttime=find(diff(Etimeindx)>1);
-    if selecttime
-        EventEtime=Etimeindx(1:selecttime(1)-1);
+    if TorqueFlage
+        [maxtorque,maxIndx]=max(Data(:,2));
+        EtimeindxT=find(Stimeindx> maxIndx);
+        EventEtime=maxIndx:Stimeindx(EtimeindxT(1));
     else
-        EventEtime=Etimeindx;
+        if selecttime
+            EventEtime=Etimeindx(1:selecttime(1)-1);
+        else
+            EventEtime=Etimeindx;
+        end
     end
     Events.ConstantTime=ConstantTime;
     Events.EventEtime=EventEtime;
-    
     
 end
