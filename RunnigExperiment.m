71229dd25280f0ff778701b63898c47a4faf1db3
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2023-01-19 19:30:20 -0500

    Used Biodex data instead of goniometers and run opt for ankel not lower than -35 deg
    In 35 degree tendon length gets spike and mess up with solution

--- RunnigExperiment.m
+++ RunnigExperiment.m
@@ -1,21 +1,21 @@
 clear all
 import org.opensim.modeling.*;
 myLog = JavaLogSink();
-Logger.addSink(myLog)
+Logger.addSink(myLog);
+
 Project='P006';
 SubjectNumber=["06","07","08","09","10","11","12","13","14","15"];
-% SubjectNumber=["08","09","10","11","12","13","14","15"];
+% SubjectNumber=["06"];
 
 SubjectNumber=append("T0",SubjectNumber);
 Data.whichleg="l";
 Data.joints=["ground_pelvis","hip","walker_knee","patellofemoral","ankle","mtp","subtalar"];
 Data.joints(2:end)=addingleg(Data.joints(2:end),Data.whichleg);
-% Data.Weldjoints=["ground_pelvis","hip","mtp","subtalar"];
-Data.Weldjoints=["ground_pelvis","mtp","subtalar"];
+Data.Weldjoints=["ground_pelvis","hip","mtp","subtalar"];
 Data.Weldjoints(2:end)=addingleg(Data.Weldjoints(2:end),Data.whichleg);
 Data.bodies=["pelvis","femur","tibia","patella","talus","calcn","toes"];
 Data.bodies(2:end)=addingleg(Data.bodies(2:end),Data.whichleg);
-
+maxpanlt=-15/180*pi();
 
 Data.DeGrooteflage=1;
 % Hipangle=0;%deg
@@ -31,14 +31,17 @@ Data.PassiveFiberBound=[0.05,0.9];
 Data.TendonStrainBound=[0.01,0.1];
 
 
+% Joints=["Knee","Ankle"];
 Joints=["Ankle","Knee"];
 Terials3=["L1","L2","L3"];
-
+txtBasepath=append('C:\MyCloud\OneDriveUcf\Real\Simulation\Source','\',Project)
+diarydir=append(txtBasepath,"\log.txt");
+diary(diarydir)
 %% running just Parameter optimization
 Data.justparameterflag=1;
 qe=1;
 for S=1:length(SubjectNumber)
-    Basepath=append(['C:\MyCloud\OneDriveUcf\Real\Simulation\Source'],'\',Project,'\',SubjectNumber(S));
+    Basepath=append('C:\MyCloud\OneDriveUcf\Real\Simulation\Source','\',Project,'\',SubjectNumber(S));
     Pardata=importdata(append(Basepath,"\Data\Parameters.csv"));
     psname=append(Project,'_',SubjectNumber(S));
     Data.optForce=Pardata.data(6);
@@ -47,7 +50,7 @@ for S=1:length(SubjectNumber)
     for T1=1:length(Joints)
         if contains(Joints(T1),"Knee")
             Terials2=["H90","H55","H15"];
-            Terials2unm=[90,55,10]*3.14/180;
+            Terials2unm=[90,55,10]*pi()/180;
             %             Terials2=["H90"];
             Data.ActiveCoordinates=["knee_angle"];
             Data.ActiveCoordinates=addingleg(Data.ActiveCoordinates,Data.whichleg);
@@ -56,7 +59,7 @@ for S=1:length(SubjectNumber)
             Data.ComplianacMusclename=[];
         else
             Terials2=["K90","K45","K0"];
-            Terials2unm=[90,45,0]*3.14/180;
+            Terials2unm=[90,45,0]*pi()/180;
             %             Terials2=["K90"];
             Data.ActiveCoordinates=["ankle_angle"];
             Data.ActiveCoordinates=addingleg(Data.ActiveCoordinates,Data.whichleg);
@@ -93,35 +96,57 @@ for S=1:length(SubjectNumber)
                 filename=append(psname,'_',upper(Data.whichleg),Joints(T1),'_',Terials2(T2),'_',Terials3(T3));
                 Data.(filename).RefStatepath=append(Basepath,'\Data\',filename,'_Motion.mot');
                 Data.(filename).RefControlpath=append(Basepath,'\Data\',filename,'_Torque.sto');
-                Data.(filename).ParamSimulPath=append(Basepath,'\Parameterestimation\Parameter_Opt_',filename,'.sto');
+                paramdir=fullfile(Basepath,'Parameterestimation');
+                [r,e]=mkdir(paramdir);
+                Data.(filename).TorqeSimulPath=append(paramdir,'\Torque_Opt_',filename,'.sto');
+                Data.(filename).ParamSimulPath=append(paramdir,'\Parameter_Opt_',filename,'.sto');
                 Data.(filename).ModelPath=append(Basepath,'\Model\',filename,'.osim');
-                StateDataTable=TableProcessor(Data.(filename).RefStatepath);
-                %                 HipAngle=StateDataTable.process.getDependentColumnAtIndex(0).getAsMat();
-                %                 KneeAngle=StateDataTable.process.getDependentColumnAtIndex(1).getAsMat();
-                %                 AnkleAngle=StateDataTable.process.getDependentColumnAtIndex(2).getAsMat();
-                %                 BiodexAngle=StateDataTable.process.getDependentColumnAtIndex(3).getAsMat();
+                Statestor=Storage(Data.(filename).RefStatepath);
+                StateDataTable=TableProcessor(Data.(filename).RefStatepath).process;
+                HipAngle=StateDataTable.getDependentColumnAtIndex(0).getAsMat();
+                KneeAngle=StateDataTable.getDependentColumnAtIndex(1).getAsMat();
+                AnkleAngle=StateDataTable.getDependentColumnAtIndex(2).getAsMat();
+                BiodexAngle=StateDataTable.getDependentColumnAtIndex(3).getAsMat();
+                kneelabe=StateDataTable.getColumnLabel(1);
+                anklelabe=StateDataTable.getColumnLabel(2);
+                if contains(Joints(T1),"Knee")
+                    StateDataTable.setColumnLabel(3,kneelabe)
+                    Etimeindx=StateDataTable.process.getNumRows();
+                else
+                    StateDataTable.setColumnLabel(3,anklelabe)
+                    Anklecoord =  osimmodel.getCoordinateSet().get(Data.ActiveCoordinates);
+                    Etimeindx=find(BiodexAngle>= maxpanlt);
+                end
+                for tt=1:3
+                    StateDataTable.removeColumnAtIndex(0)
+                end
+                StateDataTable.removeTableMetaDataKey('nColumns')
+                StateDataTable.addTableMetaDataString('nColumns','2')
                 %                 Data.(filename).Hipangle=mean(HipAngle);
                 %                 Meanknee=mean(KneeAngle);
                 %                 if Meanknee< 0 Meanknee=0; end
                 %                 Data.(filename).Kneeangle= 1.57;
                 %                 Data.(filename).Ankleangle=mean(AnkleAngle);
                 
-                Etimeindx=StateDataTable.process.getNumRows();
                 
-                ControlDataTable=TableProcessor(Data.(filename).RefControlpath);
-                StateSolutionTable=StateDataTable.process;
-                ControlSolutionTable=ControlDataTable.process;
-                Data.Etime=double(StateSolutionTable.getIndependentColumn().get(Etimeindx-1));
+                
+                ControlDataTable=TableProcessor(Data.(filename).RefControlpath).process;
+                StateSolutionTable=StateDataTable;
+                ControlSolutionTable=ControlDataTable;
+
+                Data.Etime=double(StateSolutionTable.getIndependentColumn().get(Etimeindx(end)-1));
+%                 [kneeTrackingSolution]=TorqueSimulation(StateDataTable,osimmodel,filename,Data);
                 [kneeTrackingParamSolution]=ParameterEstimation(StateSolutionTable,ControlSolutionTable,osimmodel,combinedname,filename,Data);
-                fprintf('Optimazation of %s is done \n',filename);
                 osimmodel=changemodelproperty(osimmodel,filename,Data,1);
                 qe=qe+1;
             end
         end
     end
 end
-
+diary off
+Logger.removeSink(myLog);
 save([Basepath '\SimData.mat'],'Data');
+
 % Plotting()
 function [data]= addingleg(data,whichleg)
 for y=1:length(data)
