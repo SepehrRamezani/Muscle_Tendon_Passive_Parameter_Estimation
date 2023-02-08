7d0fe89c32f6588178a2c2db1a0aa31bb77e6f5d
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2023-02-08 12:42:54 -0500

    finalized the function to plot the passive torque contribution for knee and ankle

--- Passiveforcegenerate.m
+++ Passiveforcegenerate.m
@@ -1,56 +1,54 @@
 function Passiveforcegenerate(combinedname,Data,osimmodel)
 import org.opensim.modeling.*;
-Kneerange=deg2rad(90);
 hiplable=append('hip_',Data.whichleg);
 kneelable=append('knee_angle_',Data.whichleg);
 anklelable=append('ankle_angle_',Data.whichleg);
 modeljointSet=osimmodel.getJointSet();
 
 state=osimmodel.initSystem();
+muscname=[];
 for i=0:1:osimmodel.getMuscles().getSize()-1
+
     k=0;
     u=0;
     step=30;
     %% finding minimum MTL
     %%Knee deformation
     CurrentMuscle=osimmodel.getMuscles().get(Data.SimMusclename(i+1));
-    if sum(strcmp(char(CurrentMuscle.getName),Data.ComplianceTendon))&& sum(contains(Data.ActiveCoordinates,'ankle_angle'))
-        AnkleCoor=osimmodel.updCoordinateSet().get(Data.ActiveCoordinates);
-        rmax=AnkleCoor.getRangeMax();
-        rmin=AnkleCoor.getRangeMin();
-%         Kneecoord.setValue(state, Kneeangle);
-        for q=rmin:(rmax-rmin)/step:rmax
-            u=u+1;
-            AnkleCoor.setValue(state, q);
-            angle(u)=Kneecoord.getValue(state);
-            osimmodel.realizePosition(state);
-            musclelength(u)=CurrentMuscle.getLength(state);
-            Passivefiberforce(u)=CurrentMuscle.getPassiveFiberForce(state);
-            Tendonforce(u)=CurrentMuscle.getTendonForce(state);
-            alpha(u)=CurrentMuscle.getPennationAngle(state);
-        end
-    else
-        Kneecoord=osimmodel.updCoordinateSet().get(Data.ActiveCoordinates);
-        for q=0:Kneerange/step:Kneerange
-            k=k+1;
-            Kneecoord.setValue(state, q);
-            angle(k,i)=Kneecoord.getValue(state);
-            osimmodel.realizePosition(state);
-            
-            degmus=DeGrooteFregly2016Muscle.safeDownCast(CurrentMuscle);
-            musclelength(k,i)=degmus.getLength(state);
-%             osimmodel.assemble(state)
-            osimmodel.equilibrateMuscles(state)
-%             osimmodel.realizeDynamics(state)
-            Passivefiberforce(k,i)=degmus.getPassiveFiberForce(state);
-            Tendonforce(k,i)=degmus.getTendonForce(state);
-            alpha(k,i)=degmus.getPennationAngle(state);
-        end
+    muscname=[muscname string(CurrentMuscle.getName())];
+    Currentcoord=osimmodel.updCoordinateSet().get(Data.ActiveCoordinates);
+    rmax=Currentcoord.getRangeMax();
+    rmin=Currentcoord.getRangeMin();
+    %         Kneecoord.setValue(state, Kneeangle);
+    for q=rmin:(rmax-rmin)/step:rmax
+        k=k+1;
+        Currentcoord.setValue(state, q);
+        angle(k,i+1)=Currentcoord.getValue(state);
+        osimmodel.realizePosition(state);
+        
+        degmus=DeGrooteFregly2016Muscle.safeDownCast(CurrentMuscle);
+        musclelength(k,i+1)=degmus.getLength(state);
+        %             osimmodel.assemble(state)
+        osimmodel.equilibrateMuscles(state)
+        musclemomentarm(k,i+1)=CurrentMuscle.get_GeometryPath().computeMomentArm(state,Currentcoord);
+        %             osimmodel.realizeDynamics(state)
+        Passivefiberforce(k,i+1)=degmus.getPassiveFiberForce(state);
+        Tendonforce(k,i+1)=degmus.getTendonForce(state);
+        alpha(k,i+1)=degmus.getPennationAngle(state);
     end
-    differ=Passivefiberforce.*cos(alpha)-Tendonforce;
-    plot(rad2deg(angle)',[Passivefiberforce'])
-    Muscleinfo.MinMTLength(i+1)=min(musclelength);
-    
-   
 end
-osimmodel.initSystem();
+differ=Passivefiberforce.*cos(alpha)-Tendonforce;
+if differ > 0.01
+    fprintf('in %s muscle %s',combinedname,string(CurrentMuscle.getName()));
+end
+%     plot(rad2deg(angle(:,i+1))',[Passivefiberforce(:,i+1)'])
+muscletorque=Tendonforce.*musclemomentarm;
+maxpasivetorque=max(abs(muscletorque),[],1);
+t = tiledlayout(1,1,'TileSpacing','compact');
+ax1 = nexttile;
+h=pie(ax1,maxpasivetorque);
+ax1.Colormap = hsv(numel(h)/2);
+lgd=legend(muscname);
+lgd.Layout.Tile = 'east';
+title(combinedname)
+end
