54623d3a712404ea973275f6df9c534cd58152bd
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2023-02-16 14:07:48 -0500

    Initializing the the function
    c3dtotrc is for making trc and grf from c3d file
    Roboticleg_exp is getting data and sorting it
    RoboticlegOpt is optimazation function

---
+++ Roboticleg_exp.m
@@ -0,0 +1,83 @@
+clear all
+% close all
+import org.opensim.modeling.*;
+Trc_path=['C:\MyCloud\OneDriveUcf\Real\Simulation\Passive_Parameter_prediction\Robotic_Leg\Data'];
+listing = dir(Trc_path);
+plotfalg=0;
+IKflag=1;
+Mnames=[];
+Tnames=[];
+for i=1:length(listing)
+    curname=string(listing(i,1).name);
+    if contains(curname,"Marker")&contains(curname,"Knee")
+        Mnames=[Mnames; curname];
+    elseif contains(curname,"Torque")&contains(curname,"Knee")
+        Tnames=[Tnames; curname];
+    else
+    end
+end
+
+modeldir=append(Trc_path,'/../FinalModel.osim');
+model=Model(modeldir);
+for S=1:length(Mnames)
+    Trialnames= erase(Mnames(S),'_Marker.trc');
+    markerdir=fullfile(Trc_path,Mnames(S));
+    motion = MarkerData(markerdir);
+    outdir=fullfile(Trc_path,append(Trialnames,"_IK.mot"));
+    if IKflag
+    ikTool=InverseKinematicsTool(fullfile(Trc_path,'IK_Setup.xml'));
+    ikTool.setName(Trialnames)
+    ikTool.setModel(model);
+    ikTool.setResultsDir(Trc_path)
+    %             ikTool.set_report_marker_locations(true);
+    ikTool.setMarkerDataFileName(markerdir);
+    ikTool.setStartTime(motion.getStartFrameTime());
+    ikTool.setEndTime(motion.getLastFrameTime());
+
+    ikTool.setOutputMotionFileName(outdir);
+    ikTool.run();
+
+    %     ik_marker_Error_file = strrep(outdir, 'IK.mot', 'ik_marker_errors.sto');
+    ik_marker_Error_file = strrep(outdir, 'IK.mot', 'ik_marker_errors.sto');
+    ik_marker_Error=importdata(ik_marker_Error_file);
+    marker_error_RMS=mean(ik_marker_Error.data(:,3));
+    [Max_error_RMS,Indx]=max(ik_marker_Error.data(:,3));
+    if marker_error_RMS>0.001
+        fprintf('Avg and Max marker_RMS of %s is %3.4f and %3.4f at %3.4f\n',Mnames(S),marker_error_RMS,Max_error_RMS,ik_marker_Error.data(Indx,1));
+        BadIKData.name=Mnames;
+        BadIKData.data=marker_error_RMS;
+    end
+
+    end
+    ik_result= TableProcessor(outdir).process; 
+    torque_result=TableProcessor(fullfile(Trc_path,Tnames(S))).process;
+%     torque_result=importdata(fullfile(Trc_path,Tnames(S)));
+    kneeangle=mean(ik_result.getDependentColumnAtIndex(7).getAsMat());
+    Hipangle=mean(ik_result.getDependentColumnAtIndex(6).getAsMat());
+    torque=mean(torque_result.getDependentColumnAtIndex(0).getAsMat());
+    FinalData(S,:)=[kneeangle,Hipangle,torque];
+
+end
+
+indx35=find(FinalData(:,2)<40& FinalData(:,2)>30);
+Data35=sortrows(FinalData(indx35,:),1);
+indx45=find(FinalData(:,2)<50& FinalData(:,2)>40);
+Data45=sortrows(FinalData(indx45,:),1);
+indx70=find(FinalData(:,2)<75& FinalData(:,2)>65);
+Data70=sortrows(FinalData(indx70,:),1);
+indx90=find(FinalData(:,2)<95& FinalData(:,2)>85);
+Data90=sortrows(FinalData(indx90,:),1);
+if plotfalg
+plot(Data35(:,1),Data35(:,3))
+hold on
+plot(Data45(:,1),Data45(:,3))
+plot(Data70(:,1),Data70(:,3))
+plot(Data90(:,1),Data90(:,3))
+legend(string(floor([Data35(1,2),Data45(1,2),Data70(1,2),Data90(1,2)])))
+hold off
+end
+
+time90=Data90(:,1)*20/90;
+newtim90=0:20/(4*length(time90)):20;
+newData90=interp1(time90,Data90,newtim90,"spline","extrap");
+% % c3dtotrc();
---
+++ c3dtotrc.m
@@ -0,0 +1,95 @@
+clear all
+txtdir=['C:\MyCloud\OneDriveUcf\Real\Simulation\Passive_Parameter_prediction\Robotic_Leg\Data'];
+listing = dir(txtdir);
+names=[];
+Title='\ninDegrees=no\nnum_controls=1\nnum_derivatives=0\nDataType=double\nversion=3\nnRows=%d\nnColumns=%d\nendheader\n';
+
+for i=1:length(listing)
+    curname=string(listing(i,1).name);
+%     if contains(curname,"Trial")&~contains(curname,"Knee")&~contains(curname,"Marker")&~contains(curname,"Torque")
+    if contains(curname,"Knee")&contains(curname,".c3d")
+        names=[names; curname];
+    end
+end
+
+% Knee=filedata.Knee;
+% Ankle=filedata.Ankle;
+% Trial=filedata.Trial;
+% Subject=filedata.Subject;
+filedata.trial=[];
+newFPS=100;
+RMatrix=[0 0 1; ...
+    1 0 0; ...
+    0 1 0];
+% angle=[]
+for S=1:length(names)
+    %             trial_name=char(filedata.trialas(S));
+    %             indxuderline=strfind(trial_name,'_');
+    %             Subject=trial_name(1:indxuderline(1)-1);
+    %             fname=erase(trial_name,append(Subject,"_"));
+    %             Trc_path=append(filedata.Basepath,'\Moca\',Subject,'\');
+    %             fname=append(Knee(K),"_",Ankle(A),"_L_",Trial(T));
+    %             fullname=filedata.trialas(S);
+    filedir=char(fullfile(txtdir,names(S)));
+    markdatastruct = c3d_getdata(filedir, 0);
+    Torque_volt=[markdatastruct.analog_data.Time  markdatastruct.analog_data.Channels.Sensor_16_EMG16];
+    oldFPS=markdatastruct.marker_data.Info.frequency;
+    Markerset=fieldnames(markdatastruct.marker_data.Markers);
+    Markerset=Markerset(~contains(Markerset,'C_'));
+    % checking if US5 and US 4 is swapped
+    MarkerData=[];
+    MarkerDatare=[];
+    rm=length(Markerset);
+    Markersetnew=Markerset;
+    [endtimeindx,cc]=size(markdatastruct.marker_data.Markers.(Markerset{1}));
+    for i = 1:rm
+        RawMarker=markdatastruct.marker_data.Markers.(Markerset{i})*RMatrix;
+        zeronumbers=find(RawMarker==0);
+        if zeronumbers>1
+            endtimeindx=min(zeronumbers(1)-1,endtimeindx);
+        end
+        if isempty(zeronumbers) zeronumbers=0; end
+        if zeronumbers(1)~=1
+            [bb,aa] = butter(2, 0.05,'low');
+            MarerDatafilt=filtfilt(bb,aa,RawMarker);
+            MarkerData =[MarkerData MarerDatafilt];
+        else
+            MarkerDatare=[MarkerDatare string(Markerset{i})];
+            fprintf('Warning: %s has removed from %s \n',string(Markerset{i}),fullname);
+            Markersetnew=Markerset(~contains(Markerset,MarkerDatare));
+        end
+    end
+    markerinfo=markdatastruct.marker_data.Info;
+    frames_oldFPS=markerinfo.First_Frame:markerinfo.Last_Frame;
+    time_oldFPS=frames_oldFPS/oldFPS;
+    endframe_oldFPS=frames_oldFPS(endtimeindx);
+    Etime=time_oldFPS(endtimeindx);
+    Stime=time_oldFPS(1);
+    time_newFPS=Stime:1/newFPS:Etime;
+    MarkerData_newFPS=interp1(time_oldFPS,MarkerData,time_newFPS,'linear');
+    [r,c]=size(MarkerData_newFPS);
+
+    markdatastruct.marker_data.Info.frequency=newFPS;
+    if markdatastruct.marker_data.Info.First_Frame>1
+        fprintf('Warning: First frame of %s changed to 1 \n',names(S));
+    end
+
+    markdatastruct.marker_data.Time= time_newFPS;
+    MarkerData_newFPS=[time_newFPS' MarkerData_newFPS];
+    markerinfo.Last_Frame=r;
+    markerinfo.First_Frame=1;
+    markerinfo.NumFrames=markerinfo.Last_Frame-markerinfo.First_Frame+1;
+    generate_Marker_Trc(Markersetnew,MarkerData_newFPS,markerinfo);
+    %save force
+    delimiterIn='\t';
+    Dataheaderforce=['time' delimiterIn '/forceset/knee_act'];
+%     calibration equation for voltage to torque
+    Torqueinterp=interp1(Torque_volt(:,1),Torque_volt(:,2),time_newFPS','linear','extrap'); %Interpolates data to match sampling time to desierd sampling time
+    Torque=(Torqueinterp-0.168)*2/0.168*67;
+    TorqueData=[time_newFPS' Torque];
+    [TFr,TFc]=size(TorqueData);
+    Titledata=[TFr,TFc];
+    F_fnames= strrep(names(S),'.c3d','_Torque.mot');
+    makefile(txtdir,F_fnames,Title,Titledata,Dataheaderforce,TorqueData,7,delimiterIn);
+
+end
