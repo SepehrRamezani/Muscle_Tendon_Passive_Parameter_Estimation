559acea0ea750b519344e9ba87ec1faaa63812d5
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2023-02-17 21:24:38 -0500

    Made a torque estimation function

---
+++ Roboticleg_TorqueSimulation.m
@@ -0,0 +1,76 @@
+function [kneeTrackingSolution]=Roboticleg_TorqueSimulation(tableProcessor,osimmodel,Coordlable,Data)
+% ModelPath=[cd '\..\ModelGenerator\OneDOF_Knee_DeGroote.osim'];
+Solverinterval=Data.TorqueSolverinterval;
+ Stime=Data.(combinedname).Stime;
+Etime=Data.(combinedname).Etime;
+import org.opensim.modeling.*;
+% myLog = JavaLogSink();
+% Logger.addSink(myLog);
+%% Initialze parameters
+% osimmodel = Model(ModelPath);
+ControlWight=1;
+StateWeight = 10.0/osimmodel.getNumCoordinates();
+GlobalstateTrackingWeight = 1;
+% Etime=20;
+% Solverinterval=10;
+%% Import reference state
+% tableProcessor = TableProcessor('referenceCoordinates.sto');
+% tableProcessor.append(TabOpLowPassFilter(6));
+%% make a tracking problem 
+track = MocoTrack();
+track.setName('kneeTracking');
+modelProcessor = ModelProcessor(osimmodel);
+track.setModel(modelProcessor);
+track.setStatesReference(TableProcessor(tableProcessor));
+track.set_states_global_tracking_weight(GlobalstateTrackingWeight);
+track.set_allow_unused_references(false);
+track.set_track_reference_position_derivatives(true);
+track.set_apply_tracked_states_to_guess(true);
+track.set_initial_time(Stime);
+track.set_final_time(Etime);
+stateWeights = MocoWeightSet();
+if contains(Data.ActiveCoordinates,"Hip_Joint")
+    stateWeights.cloneAndAppend(MocoWeight('Hip',StateWeight));    
+else
+    stateWeights.cloneAndAppend(MocoWeight('Knee',StateWeight));
+end
+track.set_states_weight_set(stateWeights);
+study = track.initialize();
+%% Updating problem
+problem = study.updProblem();
+model = modelProcessor.process();
+model.initSystem();
+%% add control costfunction
+effort = MocoControlGoal.safeDownCast(problem.updGoal('control_effort'));
+% effort.setWeight(1);
+for corindx = 1:length(Data.ActiveCoordinates)
+CoordinateActuatorName=append('/forceset/',Data.ActiveCoordinates,'_act');
+effort.setWeightForControl(CoordinateActuatorName,ControlWight);
+end
+
+%%% disable muscles
+effort.setExponent(2);
+effort.setDivideByDisplacement(false);
+%% Bounderies
+problem.setStateInfo(append('/jointset/ankle_',Data.whichleg,'/',anklelable,'/value'),[Anklecoord.getRangeMin(), Anklecoord.getRangeMax()]);
+%% Defining Solver 
+solver = study.initCasADiSolver();
+solver.set_num_mesh_intervals(Solverinterval);
+solver.set_verbosity(2);
+solver.set_optim_solver('ipopt');
+solver.set_optim_convergence_tolerance(1e-3);
+solver.set_optim_constraint_tolerance(1e-1);
+solver.set_optim_max_iterations(3000);
+solver.set_implicit_auxiliary_derivatives_weight(0.00001)
+solver.set_parameters_require_initsystem(false);
+solver.resetProblem(problem);
+if isfile(Data.(Coordlable).TorqeSimulPath)
+     solver.setGuessFile(Data.(Coordlable).TorqeSimulPath);
+else
+%     solver.setGuessFile([cd '\TorqueSimulation\Tracking_Initial_Guess.sto']);
+end
+kneeTrackingSolution = study.solve();
+kneeTrackingSolution.write(Data.(Coordlable).TorqeSimulPath);
+% study.visualize(kneeTrackingSolution);
+end
+
--- Roboticleg_exp.m
+++ Roboticleg_exp.m
@@ -1,83 +1,48 @@
 clear all
 % close all
 import org.opensim.modeling.*;
-Trc_path=['C:\MyCloud\OneDriveUcf\Real\Simulation\Passive_Parameter_prediction\Robotic_Leg\Data'];
+Trc_path=['C:\MyCloud\OneDriveUcf\Real\Simulation\Passive_Parameter_prediction\Robotic_Leg\'];
 listing = dir(Trc_path);
 plotfalg=0;
 IKflag=1;
 Mnames=[];
 Tnames=[];
-for i=1:length(listing)
-    curname=string(listing(i,1).name);
-    if contains(curname,"Marker")&contains(curname,"Knee")
-        Mnames=[Mnames; curname];
-    elseif contains(curname,"Torque")&contains(curname,"Knee")
-        Tnames=[Tnames; curname];
-    else
-    end
+
+% myLog = JavaLogSink();
+% Logger.addSink(myLog);
+
+runver="E3";
+% SubjectNumber=["06","07","08","09","10","11","12","13","14","15"];
+SubjectNumber=["10"];
+
+
+SubjectNumber=append("T0",SubjectNumber);
+Data.joints=["ground_pelvis","Hip_Joint","Knee_Joint","Ankle_Joint"];
+Data.Weldjoints=["ground_pelvis","Ankle_Joint"];
+Data.bodies=["Hip_Bone","Thigh","Shank","Foot"];
+Data.maxpanlt=-deg2rad(15);
+Data.DeGrooteflage=1;
+Data.TorqueSolverinterval=40;
+Data.ParamSolverinterval=50;
+Data.PassiveFiberBound=[0.01,0.9];
+Data.TendonStrainBound=[0.005,0.1];
+Data.optForce=1;
+psname=["Knee90Trial0","Knee45Trial0","Knee0Trial0"];
+for S=length(psname)
+    combinedname=psname(S);
+    Data.(combinedname).ModelPath=fullfile(Trc_path,'Model',append(combinedname,'.osim'));
+    Data.(combinedname).RefModelpath=fullfile(Trc_path,'Model','FinalModel.osim');
+    Data.(combinedname).RefStatepath=fullfile(Trc_path,'Data',append(combinedname,'_IK.mot'));
+    Data.(combinedname).RefControlpath=fullfile(Trc_path,'Data',append(combinedname,'_Torque.sto'));
+    Data.(combinedname).Hipangle=deg2rad(90);
+    Data.(combinedname).Kneeangle=0;
+    Data.(combinedname).Ankleangle=0;
+    Data.ActiveCoordinates=["Hip","Knee"];
+    osimmodel = Model(Data.(combinedname).ModelPath);
+%     osimmodel=Robotic_Modelcreator(combinedname,Data,Refmmodel);
+    StateDataTable=TableProcessor(Data.(combinedname).RefStatepath).process;
+    p=StateDataTable.getNumRows();
+    Data.(combinedname).Stime=double(StateDataTable.getIndependentColumn().get(0));
+    Data.(combinedname).Etime=double(StateDataTable.getIndependentColumn().get(p-1));
+    [kneeTrackingSolution]=Roboticleg_TorqueSimulation(StateDataTable,osimmodel,combinedname,Data);
 end
-
-modeldir=append(Trc_path,'/../FinalModel.osim');
-model=Model(modeldir);
-for S=1:length(Mnames)
-    Trialnames= erase(Mnames(S),'_Marker.trc');
-    markerdir=fullfile(Trc_path,Mnames(S));
-    motion = MarkerData(markerdir);
-    outdir=fullfile(Trc_path,append(Trialnames,"_IK.mot"));
-    if IKflag
-    ikTool=InverseKinematicsTool(fullfile(Trc_path,'IK_Setup.xml'));
-    ikTool.setName(Trialnames)
-    ikTool.setModel(model);
-    ikTool.setResultsDir(Trc_path)
-    %             ikTool.set_report_marker_locations(true);
-    ikTool.setMarkerDataFileName(markerdir);
-    ikTool.setStartTime(motion.getStartFrameTime());
-    ikTool.setEndTime(motion.getLastFrameTime());
-
-    ikTool.setOutputMotionFileName(outdir);
-    ikTool.run();
-
-    %     ik_marker_Error_file = strrep(outdir, 'IK.mot', 'ik_marker_errors.sto');
-    ik_marker_Error_file = strrep(outdir, 'IK.mot', 'ik_marker_errors.sto');
-    ik_marker_Error=importdata(ik_marker_Error_file);
-    marker_error_RMS=mean(ik_marker_Error.data(:,3));
-    [Max_error_RMS,Indx]=max(ik_marker_Error.data(:,3));
-    if marker_error_RMS>0.001
-        fprintf('Avg and Max marker_RMS of %s is %3.4f and %3.4f at %3.4f\n',Mnames(S),marker_error_RMS,Max_error_RMS,ik_marker_Error.data(Indx,1));
-        BadIKData.name=Mnames;
-        BadIKData.data=marker_error_RMS;
-    end
-
-    end
-    ik_result= TableProcessor(outdir).process; 
-    torque_result=TableProcessor(fullfile(Trc_path,Tnames(S))).process;
-%     torque_result=importdata(fullfile(Trc_path,Tnames(S)));
-    kneeangle=mean(ik_result.getDependentColumnAtIndex(7).getAsMat());
-    Hipangle=mean(ik_result.getDependentColumnAtIndex(6).getAsMat());
-    torque=mean(torque_result.getDependentColumnAtIndex(0).getAsMat());
-    FinalData(S,:)=[kneeangle,Hipangle,torque];
-
-end
-
-indx35=find(FinalData(:,2)<40& FinalData(:,2)>30);
-Data35=sortrows(FinalData(indx35,:),1);
-indx45=find(FinalData(:,2)<50& FinalData(:,2)>40);
-Data45=sortrows(FinalData(indx45,:),1);
-indx70=find(FinalData(:,2)<75& FinalData(:,2)>65);
-Data70=sortrows(FinalData(indx70,:),1);
-indx90=find(FinalData(:,2)<95& FinalData(:,2)>85);
-Data90=sortrows(FinalData(indx90,:),1);
-if plotfalg
-plot(Data35(:,1),Data35(:,3))
-hold on
-plot(Data45(:,1),Data45(:,3))
-plot(Data70(:,1),Data70(:,3))
-plot(Data90(:,1),Data90(:,3))
-legend(string(floor([Data35(1,2),Data45(1,2),Data70(1,2),Data90(1,2)])))
-hold off
-end
-
-time90=Data90(:,1)*20/90;
-newtim90=0:20/(4*length(time90)):20;
-newData90=interp1(time90,Data90,newtim90,"spline","extrap");
-% % c3dtotrc();
