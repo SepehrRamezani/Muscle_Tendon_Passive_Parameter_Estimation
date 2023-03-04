013539f00f9e08ace17151b1ebed1f7cab29b329
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2023-03-04 18:40:49 -0500

    Changing the boundaries of parameters and accuracy of  optimization

--- Roboticleg_ParamOpt.m
+++ Roboticleg_ParamOpt.m
@@ -20,7 +20,7 @@ end
 
 Solverinterval=Data.ParamSolverinterval;
 Stime=Data.(Coordlable).Stime;
-Etime=Data.(Coordlable).Etime;
+% Etime=Data.(Coordlable).Etime;
 import org.opensim.modeling.*;
 hiplable='/jointset/Hip_Joint/Hip/value';
 kneelable='/jointset/Knee_Joint/Knee/value';
@@ -31,7 +31,7 @@ kneelable='/jointset/Knee_Joint/Knee/value';
 ControlWight=1;
 StateWeight = 2/osimmodel.getNumCoordinates();
 GlobalstateTrackingWeight = 1;
-TotalControlWight=10;
+TotalControlWight=50;
 % Etime=20;
 % Solverinterval=10;
 %% Import reference state
@@ -48,13 +48,14 @@ track.set_allow_unused_references(false);
 track.set_track_reference_position_derivatives(true);
 track.set_apply_tracked_states_to_guess(true);
 % track.set_initial_time(Stime);
-track.set_final_time(Etime);
+% track.set_final_time(Etime);
 stateWeights = MocoWeightSet();
 statelables=StateTable.getColumnLabels;
 for i=1:statelables.size()
     statelable=string(statelables.get(i-1));
     if contains(statelable,"jointset")&contains(statelable,"speed")
         stateWeights.cloneAndAppend(MocoWeight(statelable,StateWeight/4));
+%  stateWeights.cloneAndAppend(MocoWeight(statelable,0/4));
     elseif contains(statelable,"jointset")&contains(statelable,"value")
         stateWeights.cloneAndAppend(MocoWeight(statelable,StateWeight));
     else
@@ -74,13 +75,28 @@ ConttableProcessor = TableProcessor(ControlTable);
 ContTracking.setReference(ConttableProcessor);
 ContTracking.setWeight(TotalControlWight);
 
-for i=1:length(Data.ActiveAct)    
+Minrestpos=Data.restingpos-Data.Maxdeformation;
+Minrestpos(find(Minrestpos<(Data.restingpos*.2)))=Data.restingpos(find(Minrestpos<(Data.restingpos*.2)))*.2;
+for i=1:length(Data.ActiveAct)
     Springname=string(osimmodel.getForceSet().get(Data.ActiveAct(i)).getName());
+    Maxrestpos=Data.(Coordlable).(Springname).Minlenght;
+
+    %     Minrestpos
+
     MusPath=append('/forceset/',char(Springname));
-    param = MocoParameter(append('stiffness_',char(Springname)),MusPath,'stiffness', MocoBounds(1,6000));
+    
+    param = MocoParameter(append('stiffness_',char(Springname)),MusPath,'stiffness', MocoBounds(Data.Stiffnessboundary(i,1),Data.Stiffnessboundary(i,2)));
+    param1 = MocoParameter(append('restpos_',char(Springname)),MusPath,'resting_length', MocoBounds(Minrestpos(i),Maxrestpos));
     problem.addParameter(param);
+    problem.addParameter(param1);
 end
 
+
+% param = MocoParameter('Leg_mass','/bodyset/Shank','mass', MocoBounds(0.5,2));
+% param1 = MocoParameter('Foot_mass','/bodyset/Foot','mass', MocoBounds(0.3,.9));
+% 
+% problem.addParameter(param);
+% problem.addParameter(param1);
 
 for corindx = 1:length(Data.ActiveCoordinates)
     %% force reference labaling
@@ -100,12 +116,13 @@ effort = MocoControlGoal.safeDownCast(problem.updGoal('control_effort'));
 effort.setWeight(0);
 
 %% Defining Solver
+% solver=study.initTropterSolver();
 solver = study.initCasADiSolver();
 solver.set_num_mesh_intervals(Solverinterval);
 solver.set_verbosity(2);
 solver.set_optim_solver('ipopt');
-solver.set_optim_convergence_tolerance(2e-5);
-solver.set_optim_constraint_tolerance(1e-2);
+solver.set_optim_convergence_tolerance(1e-5);
+solver.set_optim_constraint_tolerance(1e-32);
 solver.set_optim_max_iterations(6000);
 solver.set_implicit_auxiliary_derivatives_weight(0.00001);
 solver.set_parameters_require_initsystem(false);
