41756f2140b07544a2181bbda732fc1505b6328f
Sepehr <64617597+SepehrRamezani@users.noreply.github.com>
2023-03-16 22:08:59 -0400

    Optimized the code for multiplot

--- PlottingExp.m
+++ PlottingExp.m
@@ -9,11 +9,11 @@ OptTendonStrain=[];
 % Muscname=Data.SimMusclename;
 MarkerSize=6;
 OptPassiveFiber=[];
-SubjectNumber=["06","07","08","09","10","11","12","13","14","15"];
-% SubjectNumber=["10"];
+% SubjectNumber=["06","07","08","09","10","11","12","13","14","15"];
+SubjectNumber=["10"];
 SubjectNumber=append('T0',SubjectNumber);
-Joints=["Knee","Ankle"];
-% Joints=["Ankle","Knee"];
+% Joints=["Knee","Ankle"];
+Joints=["Ankle","Knee"];
 Joints=append("L",Joints);
 Data.runver
 % Terials2=["K90"];
@@ -23,14 +23,14 @@ Terials3=["L1","L2","L3"];
 % axes1 = axes('Parent',figure1);
 % hold(axes1,'on');
 % box(axes1,'on');
-% figure2=figure;
-% axes2 = axes('Parent',figure2);
-% hold(axes2,'on');
-% box(axes2,'on');
-% figure3=figure;
-% axes3 = axes('Parent',figure3);
-% hold(axes3,'on');
-% box(axes3,'on');
+figure2=figure;
+axes2 = axes('Parent',figure2);
+hold(axes2,'on');
+box(axes2,'on');
+figure3=figure;
+axes3 = axes('Parent',figure3);
+hold(axes3,'on');
+box(axes3,'on');
 % for i=1:7
 %    figures{i}=figure; 
 % end
@@ -39,6 +39,7 @@ newcolors = {'#F00','#F80','#000','#0B0','#00F','#50F','#A0F'};
 fildnames=fieldnames(Data);
 trialnames=string(fildnames((contains(fieldnames(Data),'_L1')|contains(fieldnames(Data),'_L2')|contains(fieldnames(Data),'_L3'))));
 for S=1:length(SubjectNumber)
+
     psname=append(Project,'_',SubjectNumber(S));
     counter=0;
     OptPassiveFiber=[];
@@ -53,6 +54,8 @@ for S=1:length(SubjectNumber)
     muscle_stiffness_error=[];
     tendon_stiffness=[];
     tendon_stiffness_error=[];
+    OptTSL=[];
+    colorindx=0;
     for T1=1:length(Joints)
 
         if contains(Joints(T1),"Knee")
@@ -63,7 +66,7 @@ for S=1:length(SubjectNumber)
             anklelable=["K90","K45","K0"];
             %             Terials2=["K90"];
         end
-        OptTSL=[];
+   
         counter=0;
         for T2=1:length(Terials2)
             
@@ -93,6 +96,8 @@ for S=1:length(SubjectNumber)
                     ControlRefData.colheaders=[ControlRefData.colheaders string(ControlDataTable.getColumnLabel(iLabel))];
                 end
                 % getting optimization result
+                OptL_counter=0;
+                TSlack_counter=0;
                 compliance_tendon_counter=0;
                 stiff_tendon_counter=0;
                 OptTableData=TableProcessor(Data.(trialname).ParamSimulPath).process;
@@ -112,6 +117,9 @@ for S=1:length(SubjectNumber)
                     fprintf('optimization %s is unsuccessful \n',trialname);
                 end
                 %% getting mucsles parameters
+                c1=0.2;
+                c2=1;
+                c3=0.2;
                 Muscname=Data.(combinedname).muscle4opt;
                 MuscleInfo=Data.(combinedname).MuscleInfo;
                 for m=1:length(Muscname)
@@ -120,48 +128,50 @@ for S=1:length(SubjectNumber)
                     
                     for y=1:length(M_indx)
                         if contains(OptData.colheaders(M_indx(y)),'tendon_slack')
-                            tendod_slack_ref(m)= MuscleInfo.TSlack(indxn);
+                            TSlack_counter=TSlack_counter+1;
+                            tendod_slack_ref(counter,m)= MuscleInfo.TSlack(indxn);
                             OptTSL(counter,m)=OptData.data(1,M_indx(y));
-                        elseif contains(OptData.colheaders(M_indx(y)),'passive_fiber_')
+                            TSL_legend(counter,m)=MuscleInfo.Musclename(indxn);
+
+                            tendon_slack_Error_rigid(counter,m)=(OptTSL(counter,m)-tendod_slack_ref(counter,m))./tendod_slack_ref(counter,m).*100;
+                        elseif contains(OptData.colheaders(M_indx(y)),'passive_fiber')
                             stiff_tendon_counter=stiff_tendon_counter+1;
-                            muscle_passive_fiber_at_norm_ref(m)= MuscleInfo.Passive(indxn);
+                            muscle_passive_fiber_at_norm_ref(counter,m)= MuscleInfo.Passive(indxn);
+                            MusclepassiveOptmaxiso(counter,m)=MuscleInfo.MaxIso(indxn);
                             OptPassiveFiber(counter,stiff_tendon_counter)=OptData.data(1,M_indx(y));
+                            
+                            PassiveFiber_legend(1,m)=MuscleInfo.Musclename(indxn);
+
+                            muscle_stiffness(counter,m)=2./(OptPassiveFiber(counter,m));
+                            muscle_stiffness_ref(counter,m)=2./(muscle_passive_fiber_at_norm_ref(counter,m));
+                            muscle_stiffness_error(counter,m)=(muscle_stiffness(counter,m)-muscle_stiffness_ref(counter,m))./muscle_stiffness_ref(counter,m).*100;
+                            muscle_passive_fiber_at_norm_ref_Error(counter,m)=100*(OptPassiveFiber(counter,m)-muscle_passive_fiber_at_norm_ref(counter,m))./muscle_passive_fiber_at_norm_ref(counter,m);
                         elseif contains(OptData.colheaders(M_indx(y)),'tendon_strain')
                             compliance_tendon_counter=compliance_tendon_counter+1;
-                            tendon_strain_at_norm_ref(m)=MuscleInfo.Tstrain(indxn);
+                            tendon_strain_at_norm_ref(counter,m)=MuscleInfo.Tstrain(indxn);
                             OptTendonStrain(counter,compliance_tendon_counter)=OptData.data(1,M_indx(y));
+                            TendonStrain_legend(counter,m)=MuscleInfo.Musclename(indxn);
+                            Tenodoptmaxiso(counter,m)=MuscleInfo.MaxIso(indxn);
+                            TenodoptOptFiber(counter,m)=MuscleInfo.OptFiberL(indxn);
+                            tendon_stiffness(counter,m) = 1.1*log((1.0 + c3) / c1) ./ (1.0 + OptTendonStrain(counter,m) - c2);
+                            tendon_stiffness_ref(counter,m)=1.1*log((1.0 + c3) / c1) ./ (1.0 + tendon_strain_at_norm_ref(counter,m) - c2);
+                            tendon_stiffness_error(counter,m)=(tendon_stiffness(counter,m)-tendon_stiffness_ref(counter,m))./tendon_stiffness_ref(counter,m).*100;
+                            epsilonerror(counter,m)=(OptTendonStrain(counter,m)-tendon_strain_at_norm_ref(counter,m))./tendon_strain_at_norm_ref(counter,m).*100;
+
+                        elseif contains(OptData.colheaders(M_indx(y)),'OptL')
+                            OptL_counter=OptL_counter+1;
+                            Opt_fiber_length_ref(counter,m)=MuscleInfo.OptFiberL(indxn);
+                            OptFiberLength(counter,OptL_counter)=OptData.data(1,M_indx(y));
+                            OptFiberLength_legend(counter,m)=MuscleInfo.Musclename(indxn);
+                            Opt_fiber_length_Error(counter,m)=(OptFiberLength(counter,m)-Opt_fiber_length_ref(counter,m))./Opt_fiber_length_ref(counter,m).*100;
+
                         end
                     end
 
                 end
-                %     Coordlable(i,1)=append("Hip",num2str(Data.Hipangle(i)));
-                %% Calculating parameter error
-                if contains(Joints(T1),"Knee")
-                    Muscname_rigid_tendon=Muscname;
-                    %% Tendon Slack Length
-                    tendod_slack_rigid(counter,:)=OptTSL(counter,:);
-                    tendon_slack_Error_rigid(counter,:)=(tendod_slack_rigid(counter,:)-tendod_slack_ref)./tendod_slack_ref.*100;
-                    %% Muscle Stiffness
-                    %     muscle_stiffness=1./(1+OptParam2(i,:));
-                    muscle_stiffness(counter,:)=2./(OptPassiveFiber(counter,:));
-                    %     muscle_stiffness_ref=1./(1+muscle_passive_fiber_at_norm_ref);
-                    muscle_stiffness_ref=2./(muscle_passive_fiber_at_norm_ref);
-                    muscle_stiffness_error(counter,:)=(muscle_stiffness(counter,:)-muscle_stiffness_ref)./muscle_stiffness_ref.*100;
-                    muscle_passive_fiber_at_norm_ref_Error(counter,:)=100*(OptPassiveFiber(counter,:)-muscle_passive_fiber_at_norm_ref)./muscle_passive_fiber_at_norm_ref;
-                else
-                    Muscname_comp_tendon=Muscname;
-                    tendod_slack_comp(counter,:)=OptTSL(counter,:);
-                    tendon_slack_Error_comp(counter,:)=(tendod_slack_comp(counter,:)-tendod_slack_ref)./tendod_slack_ref.*100;
-                    c1=0.2;
-                    c2=1;
-                    c3=0.2;
-                    tendon_stiffness(counter,:) = 1.1*log((1.0 + c3) / c1) ./ (1.0 + OptTendonStrain(counter,:) - c2);
-                    %tendon_stiffness=1./(1+OptParam3(i,:));
-                    %tendon_stiffness_ref=1./(1+tendon_strain_at_norm_ref);
-                    tendon_stiffness_ref=1.1*log((1.0 + c3) / c1) ./ (1.0 + tendon_strain_at_norm_ref - c2);
-                    tendon_stiffness_error(counter,:)=(tendon_stiffness(counter,:)-tendon_stiffness_ref)./tendon_stiffness_ref.*100;
-                    epsilonerror(counter,:)=(OptTendonStrain(counter,:)-tendon_strain_at_norm_ref)./tendon_strain_at_norm_ref.*100;
-                end
+             
+
+
                 %% Getting ref and FD states
                 Header=OptData.colheaders;
                 Ddata=OptData.data;
@@ -171,7 +181,7 @@ for S=1:length(SubjectNumber)
                 ActiveCoordinates=Data.(combinedname).ActiveCoordinates;
                 Angle_idx=find(contains(Header,append(ActiveCoordinates,'/value')));
                 Activecoor=Ddata(:,Angle_idx)*180/3.14;
-                RefAngle_idx=(contains(RefData.colheaders,append(ActiveCoordinates,'/value')));
+                RefAngle_idx=(contains(RefData.colheaders,'Biodex'));
                 RefMotionActivecoor=RefData.data(:,RefAngle_idx)*180/3.14;
                 % Toruqe
                 act_idx=find(contains(Header,append(ActiveCoordinates,'_act')));
@@ -184,41 +194,67 @@ for S=1:length(SubjectNumber)
                 vq1 = interp1(time,Activecoor,reftime);
                 vq2 = (interp1(time,TorqueActivecoor,reftime))*Data.optForce  ;
                 Error=RefMotionActivecoor-vq1;
-
-
-
-                %             color=newcolors{i};
-
-                %             plot(reftime,Error,'Color',color,'Parent',axes1);
-                %             plot(reftime,RefMotionActivecoor,'--',reftime,vq1,'Color',color,'Parent',axes2);
-                %             plot(reftime,RefTorqueActivecoor*Data.optForce,'--',reftime,vq2,'Color',color,'Parent',axes3)
-                %             Lege=[Lege ,append(trialname,"-Exp"), append(trialname,"-Sim")];
+                colorindx=colorindx+1;
+                if colorindx > 7 colorindx=1; end
+                color=newcolors{colorindx};
+%                 plot(reftime,Error,'Color',color,'Parent',axes1);
+                plot(reftime,RefMotionActivecoor,'--',reftime,vq1,'Color',color,'Parent',axes2);
+                legend([append(trialname,"-Exp"), append(trialname,"-Sim")]);
+                plot(reftime,RefTorqueActivecoor*Data.optForce,'--',reftime,vq2,'Color',color,'Parent',axes3)
+                legend([append(trialname,"-Exp"), append(trialname,"-Sim")]);
             end
 %             Tendod_slacks.(Terials2(T2))=table2cell(table(Muscname',tendod_slack'));
 %             muscle_stiffnesss.(Terials2(T2))=table2cell(table(Muscname',muscle_stiffness'));
 %             tendod_slack=
             
         end
+        
+        
+        MutilOptPassiveFiber.(Joints(T1)).OptPassiveFiber=OptPassiveFiber;
+        MutilOptPassiveFiber.(Joints(T1)).muscle_stiffness=muscle_stiffness;
+        MutilOptPassiveFiber.(Joints(T1)).muscle_stiffness_ref=muscle_stiffness_ref;
+        MutilOptPassiveFiber.(Joints(T1)).muscle_stiffness_error=muscle_stiffness_error;
+        MutilOptPassiveFiber.(Joints(T1)).MaxIso=MusclepassiveOptmaxiso;
+        MutilOptPassiveFiber.(Joints(T1)).Legend=PassiveFiber_legend;
+        
     end
+    
     %% Tendon Slack Length
-    tendod_slack=[tendod_slack_rigid,tendod_slack_comp];
+%     tendod_slack=[tendod_slack_rigid,tendod_slack_comp];
+
     h(1)=figure;
     ttt=append(SubjectNumber(S),'-Tendon Slack length different trials');
-    Muscname=[Muscname_rigid_tendon Muscname_comp_tendon];
-    numfigs=10;
-    t=tiledlayout(numfigs/2,2);
-    multiplotting(t,Muscname_rigid_tendon,kneelable,ttt,tendod_slack_rigid,newcolors,MarkerSize,1);
-    multiplotting(t,Muscname_comp_tendon,anklelable,ttt,tendod_slack_comp,newcolors,MarkerSize,1);
-   %% Muscle-Tendon Stiffness
+    [rl,cl]=size(TSL_legend);
+    t=tiledlayout(ceil(cl/2),2);
+    multiplotting(t,TSL_legend(1,:),anklelable,ttt,OptTSL,newcolors,MarkerSize,1);
+   %% Muscle Stiffness
     h(2)=figure;
-    numfigs=10;
-    t2=tiledlayout(numfigs/2,2);
-    ttt2=append(SubjectNumber(S),'-Muscle-Tendon Stiffness different trials');
-    multiplotting(t2,Muscname_rigid_tendon,kneelable,ttt2,muscle_stiffness,newcolors,MarkerSize,1);
-    multiplotting(t2,Muscname_comp_tendon,anklelable,ttt2,tendon_stiffness,newcolors,MarkerSize,1);
+    cl=10;
+    t2=tiledlayout(ceil(cl/2),2);
+    ttt2=append(SubjectNumber(S),'Muscle Stiffness different trials');
+    if ~contains(Joints(1),'Ankle')
+        Joints(1:end)=Joints(end:-1:1);
+    end
+    musclestiffnesscoef=Tenodoptmaxiso./TenodoptOptFiber;
+    PData=MutilOptPassiveFiber.(Joints(1)).muscle_stiffness.*musclestiffnesscoef;
+    Ylable=MutilOptPassiveFiber.(Joints(1)).Legend;
+    multiplotting(t2,Ylable,anklelable,ttt2,PData,newcolors,MarkerSize,1);
     
-    
-    h(3)=figure;
+    musclestiffnesscoef1=MutilOptPassiveFiber.(Joints(2)).MaxIso./MutilOptPassiveFiber.(Joints(2)).OptPassiveFiber;
+    PData=MutilOptPassiveFiber.(Joints(2)).muscle_stiffness.*musclestiffnesscoef1;
+    Ylable=MutilOptPassiveFiber.(Joints(2)).Legend;
+    multiplotting(t2,Ylable,kneelable,ttt2,PData,newcolors,MarkerSize,1);
+    %% tendon Stiffness
+    tendocoef=Tenodoptmaxiso./OptTSL;
+    h(2)=figure;
+    ttt2=append(SubjectNumber(S),'Tendon Stiffness different trials');
+    c1=4;
+    t2=tiledlayout(ceil(cl/2),2);
+    PData=tendon_stiffness.*tendocoef;
+    Ylable=TendonStrain_legend(1,:);
+    multiplotting(t2,Ylable,anklelable,ttt2,PData,newcolors,MarkerSize,1);
+    %%
+    h(4)=figure;
     MarkerSize=10;
     t3=tiledlayout(2,2);
     nexttile
